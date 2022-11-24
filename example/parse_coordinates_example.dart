e71e021e79d7d2f6ea639a72df7020c6aa3c3e36
Dan Siwiec <daniel.siwiec@gmail.com>
2022-11-23 16:24:52 -0800

    Address publishing violations

--- example/parse_coordinates_example.dart
+++ example/parse_coordinates_example.dart
@@ -2,5 +2,5 @@ import 'package:parse_coordinates/parse_coordinates.dart';
 
 void main() {
   var location = parseCoordinates('41.40338, 2.17403');
-  print('lat: ${location.latitude}, long: ${location.longitude}');
+  print('lat: ${location?.latitude}, long: ${location?.longitude}');
 }
